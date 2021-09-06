cd44100c2d6beba1614c4fc2c02e42477bda9c58
Hamou AMROUN <hamou.amroun@gmail.com>
2021-09-06 14:32:28 +0200

    Added the bat file

---
+++ hack_intillij.bat
@@ -0,0 +1,14 @@
+rem "Suppression du contenu du dossier eval d'intelliJ"
+rd /S /Q C:\Users\utilisateur\AppData\Roaming\JetBrains\IntelliJIdea2021.1\eval
+
+rem "Suppression des lignes contenant evlsprt dans le fichier other.xml"
+type C:\Users\utilisateur\AppData\Roaming\JetBrains\IntelliJIdea2021.1\options\other.xml | findstr /v evlsprt > C:\Users\utilisateur\AppData\Roaming\JetBrains\IntelliJIdea2021.1\options\other2.xml
+
+del C:\Users\utilisateur\AppData\Roaming\JetBrains\IntelliJIdea2021.1\options\other.xml
+
+rename C:\Users\utilisateur\AppData\Roaming\JetBrains\IntelliJIdea2021.1\options\other2.xml other.xml
+
+rem "Suppression de la clef"
+reg delete HKEY_CURRENT_USER\Software\JavaSoft\Prefs\jetbrains\idea /f
+
+rem "Fin des operations"
